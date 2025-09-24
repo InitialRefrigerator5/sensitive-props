f446269bb35e31b294dce02ecbda27d47271ac17
InitialRefrigerator5 <kurts3v@outlook.com>
2025-09-24 11:48:29 +0500

    scripts/service: remove props used in stock roms
    Examples that i found
    
    ro.product.marketname
    ro.product.odm.marketname
    ro.product.product.marketname
    ro.product.system.marketname
    ro.product.system_ext.marketname
    ro.product.vendor.marketname
    persist.sys.app_resurection.enable

--- service.sh
+++ service.sh
@@ -17,12 +17,12 @@ until [ -d "/sdcard/Android" ]; do sleep 3; done
 
 ### Props ###
 hexpatch_deleteprop "LSPosed" \
-	"marketname" "custom.device" "modversion" \
+	"custom.device" "modversion" \
 	"lineage" "aospa" "pixelexperience" "evolution" "pixelos" "pixelage" "crdroid" "crDroid" "aospa" \
 	"aicp" "arter97" "blu_spark" "cyanogenmod" "deathly" "elementalx" "elite" "franco" "hadeskernel" \
 	"morokernel" "noble" "optimus" "slimroms" "sultan" "aokp" "bharos" "calyxos" "calyxOS" "divestos" \
 	"emteria.os" "grapheneos" "indus" "iodéos" "kali" "nethunter" "omnirom" "paranoid" "replicant" \
-	"resurrection" "rising" "remix" "shift" "volla" "icosa" "kirisakura" "infinity" "Infinity"
+	"rising" "remix" "shift" "volla" "icosa" "kirisakura" "infinity" "Infinity"
 
 # Fix display properties to remove custom ROM references
 replace_value_resetprop ro.build.flavor "lineage_" ""
